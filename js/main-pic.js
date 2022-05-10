bd437cd07b100d379bf1701abb52f6449c9fc188
Jia Yu Lim <92499831+Pyrexia02@users.noreply.github.com>
2022-05-10 18:48:09 +0800

    Add files via upload

--- js/main-pic.js
+++ js/main-pic.js
@@ -1,6 +1,6 @@
 let img = document.querySelector('#above');
 let windowWidth = window.innerWidth;
-if (windowWidth<950){
+if (windowWidth<420){
     img.src = 'pics/gims-gif-vertical.gif';
 } else {
     img.src = 'pics/gims-gif.gif';
